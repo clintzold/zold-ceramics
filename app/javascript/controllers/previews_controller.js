fb8eb41cb7c87ac7d23c14068f36d90356664bdd
Clint Zold <214383878+clintzold@users.noreply.github.com>
2025-11-18 23:21:05 -0700

    change product form so multiple preview images show

--- app/javascript/controllers/previews_controller.js
+++ app/javascript/controllers/previews_controller.js
@@ -6,23 +6,38 @@ export default class extends Controller {
   connect() {
 		console.log("Howdy, man!");
   }
-	preview() {
-		let input = this.inputTarget;
-		let preview = this.previewTarget;
-		let file = input.files[0];
-		let reader = new FileReader;
-		let imgLocation = document.getElementById("imagesInput")
-
-		reader.onloadend = function () {
-			preview.style.height = "150px";
-			preview.src = reader.result;
-		};
-
-		if (file) {
-			reader.readAsDataURL(file);
-		} else {
-			preview.src = "";
-		}
-
-	}
-}
+//	preview() {
+//		let input = this.inputTarget;
+//		let preview = this.previewTarget;
+//		let file = input.files[0];
+//		let reader = new FileReader;
+//		let imgLocation = document.getElementById("imagesInput")
+//
+//		reader.onloadend = function () {
+//			preview.style.height = "150px";
+//			preview.src = reader.result;
+//		};
+//
+//		if (file) {
+//			reader.readAsDataURL(file);
+//		} else {
+//			preview.src = "";
+//		}
+//
+//	}
+//}
+preview() {
+        this.previewTarget.innerHTML = "" // Clear previous previews
+        Array.from(this.inputTarget.files).forEach(file => {
+          const reader = new FileReader()
+          reader.onload = (event) => {
+            const img = document.createElement("img")
+            img.src = event.target.result
+            img.style.height = "150px" 
+	    img.style.margin = "2px"
+            this.previewTarget.appendChild(img)
+          }
+          reader.readAsDataURL(file)
+        })
+      }
+    }
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,5 @@ class Product < ApplicationRecord
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
   end
-
   validates :title, :description, :price, :stock, presence: true
 end
