467c8dc708a79277257faafb31068b0776ed126e
Clint Zold <214383878+clintzold@users.noreply.github.com>
2026-01-11 20:14:07 -0700

    refactor active nav controller to scale with future link additions

--- app/javascript/controllers/active_nav_controller.js
+++ app/javascript/controllers/active_nav_controller.js
@@ -5,14 +5,9 @@ import { Controller } from "@hotwired/stimulus"
 // Responsible for highlighting navigation links based on
 // current page path.
 //
-// Could be optimized to iterate through link targets to find match
-// rather than multiple 'if else' conditions. This would make any
-// links added in future automatically dynamic simply by adding 
-// the data-target details.
-//
 export default class extends Controller {
 
-	static targets = ["homeLink", "shopLink", "cartLink"];
+	static targets = ["navLink"];
 	static classes = ["active"];
 
 	connect() {
@@ -23,13 +18,11 @@ export default class extends Controller {
 
 		const currentPath = window.location.pathname;
 
-		if (currentPath == "/home") {
-			this.homeLinkTarget.classList.add(this.activeClass);
-		} else if (currentPath == "/shop") {
-			this.shopLinkTarget.classList.add(this.activeClass);
-		} else {
-			this.cartLinkTarget.classList.add(this.activeClass);
-		};
+		this.navLinkTargets.forEach(link => {
+			if (link.pathname == currentPath) {
+				link.classList.add(this.activeClass);
+			};
+		});
 	}
 
 }
