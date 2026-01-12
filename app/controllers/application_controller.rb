885d755f42889286b51c3001cc034c9a85b607a4
Clint Zold <214383878+clintzold@users.noreply.github.com>
2026-01-11 23:55:18 -0700

    add cart counter to cart icon on navw

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :current_cart
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
@@ -14,4 +15,15 @@ class ApplicationController < ActionController::Base
     session[:cart_id] = @cart.id
   end
   helper_method :current_cart # Makes the method available in views
+
+  def cart_count
+    cart_count = 0
+    if @cart.cart_items.any?
+      @cart.cart_items.each do |item|
+        cart_count += item.quantity
+      end
+    end
+    cart_count
+  end
+  helper_method :cart_count
 end
