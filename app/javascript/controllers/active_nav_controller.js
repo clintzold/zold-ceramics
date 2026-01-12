1c17ef2311651ab4cc8dd5f4e3583b07739b437f
Clint Zold <214383878+clintzold@users.noreply.github.com>
2026-01-11 20:08:21 -0700

    add comments to active nav stimulus controller

--- app/javascript/controllers/active_nav_controller.js
+++ app/javascript/controllers/active_nav_controller.js
@@ -1,9 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="active-nav"
+//
+// Responsible for highlighting navigation links based on
+// current page path.
+//
+// Could be optimized to iterate through link targets to find match
+// rather than multiple 'if else' conditions. This would make any
+// links added in future automatically dynamic simply by adding 
+// the data-target details.
+//
 export default class extends Controller {
+
 	static targets = ["homeLink", "shopLink", "cartLink"];
 	static classes = ["active"];
+
 	connect() {
 		this.highlightActive()
 	}
