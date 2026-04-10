f2247671fb53812d65c369c359837f6261bb24d7
clint zold <214383878+clintzold@users.noreply.github.com>
2026-04-10 00:27:30 -0600

    set expiring cookie based on subscriber modal shown or not

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   allow_unauthenticated_access
+  before_action :set_subscribe_modal_status, only: [:shop]
 
   def home
   end
@@ -12,10 +13,27 @@ class PagesController < ApplicationController
   end
 
   def shop
+    @subscription = Subscription.new
     @products = Product.with_attached_images.includes(
       images_attachments: { blob: :variant_records }, 
     ).with_attached_main_image.includes(
       main_image_attachment:{blob: :variant_records}
     ).where(out_of_stock: false)
+  end
+
+  private
+
+  def set_subscribe_modal_status
+    # Checks if subscribe modal cookie already exists
+    @show_modal = cookies.signed[:modal_shown].blank?
+
+    if @show_modal
+      # Set cookie to expire in 1 week
+      cookies.signed[:modal_shown] = {
+        value: 'true',
+        expires: 1.minute.from_now,
+        httponly: true
+      }
+    end
   end
 end
--- app/javascript/controllers/slide_out_controller.js
+++ app/javascript/controllers/slide_out_controller.js
@@ -3,13 +3,22 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="slide-out"
 export default class extends Controller {
   connect() {
-    console.log(this.element)
   }
   slide(event) {
     event.preventDefault()
     this.element.classList.remove("forwards")
     void this.element.offsetWidth
     this.element.classList.add("reverse")
-    console.log(this.element)
+    this.removeElement()
+  }
+
+  removeElement() {
+    this.timer = setTimeout(() => {
+      this.element.remove()
+    }, 550)
+  }
+
+  disconnect() {
+    clearTimeout(this.timer)
   }
 }
