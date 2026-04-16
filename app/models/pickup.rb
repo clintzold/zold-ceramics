6a470ee5a2fed1f0bdaedff966dd11cc689ed613
clint zold <214383878+clintzold@users.noreply.github.com>
2026-04-15 22:24:08 -0600

    add callback methods to pickup model for email and order association

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -3,4 +3,19 @@ class Pickup < ApplicationRecord
 
   validates_presence_of :start, :end, :location, :link
 
+  before_create :add_orders
+  after_create_commit :send_notification_email
+
+  private
+
+  def add_orders
+    self.orders << Order.where(local: true)
+  end
+
+  def send_notification_email
+    self.orders.each do |order|
+      NotifierMailer.with(pickup: self, order: order).pickup_email.deliver_now
+    end
+  end
+
 end
