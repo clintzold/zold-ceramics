dbbc756a6a780ddb2774d947f4430f2daa64d253
clint zold <214383878+clintzold@users.noreply.github.com>
2026-04-15 20:59:56 -0600

    add method to send pickup notifications to local orders

--- app/models/pickup.rb
+++ app/models/pickup.rb
@@ -9,7 +9,11 @@ class Pickup
 
   validates_presence_of :start, :end, :location, :link
 
-  def send_email
+  def send_emails
+    orders = Order.where(local: true)
+    orders.each do |order|
+      NotifierMailer.with(pickup: self, order: order).pickup_email.deliver_later
+    end
   end
 
 end
