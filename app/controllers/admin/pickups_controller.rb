dea442b9bdb9e104ad9fa5cbd1d1253777c54c94
clint zold <214383878+clintzold@users.noreply.github.com>
2026-04-18 01:05:45 -0600

    prepare pickups controller actions for turbo_stream requests

--- app/controllers/admin/pickups_controller.rb
+++ app/controllers/admin/pickups_controller.rb
@@ -1,26 +1,43 @@
 class Admin::PickupsController < ApplicationController
   def index
     @pickups = Pickup.all
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: turbo_stream.update(
+          "email_modal", partial: "index", locals: { pickups: @pickups }
+        )
+      }
+    end
   end
 
   def new
     @pickup = Pickup.new
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: turbo_stream.update(
+          "email_modal", partial: "new", locals: { pickup: @pickup }
+        )
+      }
+    end
   end
 
   def create
     @pickup = Pickup.create(pickup_params)
-   # respond_to do |format|
-   #   if @pickup.errors.any?
-   #     format.turbo_stream {
-   #     render turbo_stream: turbo_steam.update(
-   #       "pickup_form_errors", partial: "shared/form_errors", locals: { errors: @pickup.errors }
-   #     ), status: :unprocessable_content
-   #     }
-   #   else
-   #     format.html {
-   #       redirect_to admin_pickup_path(@pickup)
-   #     }
-   # #end
+    respond_to do |format|
+      if @pickup.errors.any?
+        format.turbo_stream {
+        render turbo_stream: turbo_steam.update(
+          "pickup_form_errors", partial: "shared/form_errors", locals: { errors: @pickup.errors }
+        ), status: :unprocessable_content
+        }
+      else
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.update(
+            "email_modal", partial: "show", locals: { pickup: @pickup }
+          )
+        }
+      end
+    end
   end
 
   def show
