e1d7951bdb60a43bf0255609ae76bc0069206805
clint zold <214383878+clintzold@users.noreply.github.com>
2026-04-19 14:43:34 -0600

    fix modal not showing

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -2,17 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="modal"
 export default class extends Controller {
-  static targets = ["modal", "button"]
+  static targets = ["modal"]
   connect() {
   }
 
   visible() {
-    console.log(this.buttonTarget)
     this.modalTarget.classList.toggle("d-none")
   }
 
-  hideButton() {
-    console.log("clicked")
-    this.buttonTarget.classList.add("opacity-0")
-  }
 }
