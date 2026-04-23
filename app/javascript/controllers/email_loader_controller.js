1eaeb956d52aec0e312273773f11b9279b90fcf2
clint zold <214383878+clintzold@users.noreply.github.com>
2026-04-22 23:58:51 -0600

    remove subscribe date from subscriptions list in admin panel

--- app/javascript/controllers/email_loader_controller.js
+++ app/javascript/controllers/email_loader_controller.js
@@ -4,7 +4,6 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["loadingOverlay", "loadingIcon", "message"]
   connect() {
-    console.log(this.messageTarget)
   }
 
   show() {
