349c88105ea54c31304c382ba6f047b4094446a3
clint zold <214383878+clintzold@users.noreply.github.com>
2026-04-23 20:56:46 -0600

    destroy shipment model

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,5 @@ class Order < ApplicationRecord
   has_secure_token :token
   enum :status, { pending: 0, paid: 1, delivered: 2 }
   has_many :order_items, dependent: :destroy
-  has_one :shipment, dependent: :destroy
   has_and_belongs_to_many :pickups
 end
