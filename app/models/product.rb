b9377021d58bf921480db8fa3ac8cf4581b30bff
clint zold <214383878+clintzold@users.noreply.github.com>
2026-04-29 11:10:35 -0600

    increase variant size for product images

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,11 +10,11 @@ class Product < ApplicationRecord
   has_many :order_items
   has_one_attached :main_image do |attachable|
     attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
-    attachable.variant :card_image, resize_to_limit: [ 450, 450 ], saver: { strip: true, quality: 50 }, format: :webp
+    attachable.variant :card_image, resize_to_limit: [ 500, 500 ], saver: { strip: true, quality: 50 }, format: :webp
   end
   has_many_attached :images do |attachable|
     attachable.variant :thumb, resize_to_limit: [ 100, 100 ]
-    attachable.variant :card_image, resize_to_limit: [ 450, 450 ], saver: { strip: true, quality: 50 }, format: :webp
+    attachable.variant :card_image, resize_to_limit: [ 500, 500 ], saver: { strip: true, quality: 50 }, format: :webp
   end
 
 
