bdc0473bdcac5ca8ae05a2e9a132f1f0276f4bdd
clint zold <214383878+clintzold@users.noreply.github.com>
2026-04-28 18:25:12 -0600

    change blob purging to background job in products#destroy_orphan

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -53,7 +53,7 @@ class Admin::ProductsController < Admin::BaseController
 
   def destroy_orphan
     @orphan = ActiveStorage::Blob.find_by(filename: params[:name] + "." + params[:format])
-    @orphan.purge if @orphan
+    @orphan.purge_later if @orphan
     head :no_content
   end
 
